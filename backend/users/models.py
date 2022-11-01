067c05c192c239fcae1a03ef946a6f45890ef62f
math-a3k <rodrigo@crowdbotics.com>
2022-11-01 20:24:16 +0000

    Update models

--- backend/users/models.py
+++ backend/users/models.py
@@ -19,10 +19,8 @@ class User(AbstractUser):
     everything that relates with an `User` is represented by this model.
     """
     name = models.CharField(null=True,blank=True,max_length=255,)
-    asss = models.BigIntegerField(null=True,blank=True,)
-    fdsfa = models.BigIntegerField(null=True,blank=True,)
-    eeaaqwq = models.BigIntegerField(null=True,blank=True,)
     xzxzxx = models.BigIntegerField(null=True,blank=True,)
     vvvvffff = models.BigIntegerField(null=True,blank=True,)
+    aaaffffddd = models.BigIntegerField(null=True,blank=True,)
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
